561b3504256c4b02dbc83dda59e0856a64e19801
vchip1 <andy.volding@gmail.com>
2023-02-12 00:30:37 -0600

    3rd Push - Completed tasks 1-3 and started on tests

--- src/main/java/org/launchcode/techjobs/oo/Job.java
+++ src/main/java/org/launchcode/techjobs/oo/Job.java
@@ -22,7 +22,7 @@ public class Job {
         nextId++;
     }
 
-    public Job(String value, Employer employer, Location location, PositionType positionType, CoreCompetency coreCompetency) {
+    public Job(String name, Employer employer, Location location, PositionType positionType, CoreCompetency coreCompetency) {
         this();
         this.name = name;
         this.employer = employer;
--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -1,7 +1,11 @@
 package org.launchcode.techjobs.oo.test;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.launchcode.techjobs.oo.*;
+
+import static org.junit.Assert.*;
 
 /**
  * Created by LaunchCode
@@ -9,4 +13,26 @@ import org.junit.runners.JUnit4;
 @RunWith(JUnit4.class)
 public class JobTest {
 
+    @Test
+    public void testSettingJobId() {
+        Job garbagePerson = new Job();
+        Job deliveryDriver = new Job();
+
+        assertNotEquals(garbagePerson, deliveryDriver);
+    }
+
+    @Test
+    public void testJobConstructorSetsAllFields() {
+        Job productTester = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+
+        assertEquals(productTester.getName(), "Product tester");
+        assertEquals(productTester.getEmployer(), "ACME");
+        assertEquals(productTester.getLocation(), "Desert");
+        assertEquals(productTester.getPositionType(), "Quality Control");
+        assertEquals(productTester.getCoreCompetency(), "Persistence");
+
+        assertTrue(productTester instanceof Job);
+
+
+    }
 }
