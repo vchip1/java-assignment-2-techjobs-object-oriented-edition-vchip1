17d7b6195375794c380d9af38ccfeca3ae17db96
vchip1 <andy.volding@gmail.com>
2023-02-11 19:52:27 -0600

    1st Push - Completed Tasks 1 and 2

--- src/main/java/org/launchcode/techjobs/oo/CoreCompetency.java
+++ src/main/java/org/launchcode/techjobs/oo/CoreCompetency.java
@@ -8,6 +8,10 @@ public class CoreCompetency {
     private static int nextId = 1;
     private String value;
 
+
+
+
+
     public CoreCompetency() {
         this.id = nextId;
         nextId++;
@@ -38,7 +42,19 @@ public class CoreCompetency {
         return Objects.hash(id);
     }
 
+
     // TODO: Use the "Generate" tool to add a getter and setter for the 'value' field but
     //  ONLY a getter for the 'id' field.
 
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public int getId() {
+        return id;
+    }
 }
